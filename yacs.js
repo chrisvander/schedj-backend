7e1d2f1658fa17c29034c7c5cb9b3f98a0200815
Chris Vanderloo <chris.vanderloo@yahoo.com>
2019-01-29 16:35:49 -0500

    wtf get outta here you dumb file

--- yacs.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app, api) {
-	app.get("/api/departments", (req,res) => 
-		api.yacs.departments((err, body) => {
-			if (!err) res.send(body);
-			else res.send(err);
-		}));
-}
